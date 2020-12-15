df690e51dce4d46b3e6d9a8ca9a90da50feb56da
chenfeifan <cff@uenit.cn>
2020-12-15 17:11:10 +0800

    init创建

---
+++ src/libs/canvas.js
@@ -0,0 +1,88 @@
+const WINDOW_SIZE = {
+    W: 1920,
+    H: 1080
+}
+const IMAGE_URL = './static/starwar'
+class Fire {
+    constructor(HID, origin, aim, window) {
+        this.origin = origin
+        this.aim = aim
+        this.window = window || WINDOW_SIZE
+        this.ctx = null
+        this.images = new Image()
+        this.explosion_speed = 0
+        this.htmlID = HID
+        this.timer = null
+        this.speed = 0.02
+    }
+    init() {
+        let t = this
+        t.createHTML()
+        t.images.src = `${IMAGE_URL}/4.png`
+        console.log(t.images.width, t.images.height)
+        t.ctx.fillStyle = '#ddd'
+        t.ctx.font = '22px Helvetica Neue'
+        t.ctx.fillText(t.origin.name, t.origin.x, t.origin.y)
+
+        t.images.onload = function () {
+            t.ctx.drawImage(
+                t.images,
+                0,
+                0,
+                t.images.width,
+                t.images.height,
+                t.origin.x,
+                t.origin.y,
+                t.images.width,
+                t.images.height)
+        }
+        this.explosion_speed += 64
+
+    }
+    createHTML() {
+        let canvas = document.createElement('canvas');
+        canvas.width = this.window.w;
+        canvas.height = this.window.h;
+        this.ctx = canvas.getContext('2d')
+        if (this.htmlID) {
+            document.querySelector(this.htmlID).innerHTML = ``
+            document.querySelector(this.htmlID).appendChild(canvas)
+        } else {
+            document.body.appendChild(canvas)
+        }
+    }
+    move() {
+        let t = this
+        if (!t.timer) {
+            t.timer = setInterval(function () {
+                t.speed = t.speed + 1
+                t.ctx.clearRect(0, 0, t.window.w, t.window.h)
+                t.ctx.fillStyle = '#ddd'
+                t.ctx.font = '22px Helvetica Neue'
+                t.ctx.fillText(t.origin.name, t.origin.x + t.speed, t.origin.y)
+
+                t.ctx.drawImage(
+                    t.images,
+                    0,
+                    0,
+                    t.images.width,
+                    t.images.height,
+                    t.origin.x + t.speed,
+                    t.origin.y,
+                    t.images.width,
+                    t.images.height)
+                console.log(t.origin.x + t.speed)
+
+            }, 1000 / 60)
+        } else {
+
+        }
+
+    }
+    close() {
+        clearInterval(this.timer)
+        this.timer =null
+    }
+}
+
+export default Fire
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,45 @@
 <template>
   <div class="home">
-  
+     <div id='canvas'></div>
+     <button @click='handleOpen'>開始</button>
+      <button @click='handleClose'>停止</button>
   </div>
 </template>
 
 <script>
+import Fire from '@/libs/canvas'
 export default {
   name: 'Home',
   components: {
  
   },
+  data () {
+    return {
+          origin:{
+            x:360,
+            y:150,
+            name:"test",
+            action:1,
+            count:20
+          },
+          target:null
+    }
+  },
   created () {
     
   },
   mounted () {
-    
+      this.target = new Fire('#canvas',this.origin,this.origin,{w:1600,h:780})
+      this.target.init()
+      console.log(this.target)
   },
   methods:{
-
+    handleOpen(){
+      this.target.move()
+    },
+    handleClose(){
+      this.target.close()
+    }
   }
 }
 </script>
